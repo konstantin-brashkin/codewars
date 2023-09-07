87c2bb8a6479cd110352e855bc25361bdc8d3c88
konstantin-brashkin <137953362+konstantin-brashkin@users.noreply.github.com>
2023-09-07 11:30:58 +0300

    Add files via upload

---
+++ 7 kyu/summing_a_numbers_digit.py
@@ -0,0 +1,22 @@
+# Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of
+# the number's decimal digits.
+#
+# For example: (Input --> Output)
+#
+# 10 --> 1
+# 99 --> 18
+# -32 --> 5
+# Let's assume that all numbers in the input will be integer values.
+
+
+# MY SOLUTION
+
+def sum_digits(number):
+    return sum(int(digit) for digit in str(number) if digit.isdigit())
+
+
+# DEMONSTRATION
+
+print(sum_digits(10))
+print(sum_digits(99))
+print(sum_digits(-32))
